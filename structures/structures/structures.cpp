bf5b8cd7e4174258b58650fa064dce68af33679f
Nikkkt <work.terpilovsky@gmail.com>
2022-11-10 00:39:00 +0200

    Procedural programming in C++

--- structures/structures/structures.cpp
+++ structures/structures/structures.cpp
@@ -73,7 +73,24 @@ private:
 	double Weight;
 };
 
+struct check {
+private:
+	string indent = "\n";
+	string head = "\t\t   Berqhotel\n\t\tGrosse Scheidegg\n\t\t3818 Grindelwald\n\t\tFamilie R.Muller";
+	string prebody = "Rech.Nr. 4572\t\t    30.07.2007/13:29:17\nBar\t\t\t\t     Tisch 7/01";
+	string body = " 2xLatte Macchiato     a     4.50  CHF    9.00\n 1xGloki               a     5.00  CHF    5.00\n 1xSchweinschnitzel    a    22.00  CHF   22.00\n 1xChasspatzli         a    18.50  CHF   18.50\n                                   --------------\n             Total   :       CHF       54.50";
+	string preend = "Incl.  7.6% MwSt    54.50 CHF:        3.85\n" + indent + "Entspricht in Euro    36.33  EUR\nEs bediente Sie: Ursula\n";
+	string end = "\t\t  MwSt Nr.: 430 234\n\t\tTel.: 033 853 67 16\n\t\tFax.: 033 853 67 19\n     E-mail: grossescheidegg@bluewin.ch";
+public:
+	void show() {
+		cout << head << indent << prebody << indent << body << indent << preend << indent << end << endl;
+	}
+};
+
 int main() {
 	MyPhone MyPhone;
 	MyPhone.ShowInfo();
+	cout << endl;
+	check ck;
+	ck.show();
 }

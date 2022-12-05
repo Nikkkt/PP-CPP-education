da6dddbd1b7c3a3ae4ec7c075b3171ded40283ae
Nikkkt <work.terpilovsky@gmail.com>
2022-12-05 16:23:37 +0200

    Procedural programming in C++

--- files/Rhymes/Rhymes/Rhymes.cpp
+++ files/Rhymes/Rhymes/Rhymes.cpp
@@ -1,7 +1,30 @@
 ﻿#include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main() {
-    string path = "C:/Users/nikit/OneDrive/Рабочий стол/Programming/C++/PP C++ education/files/db.txt";
+    setlocale(0, "");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
+    string term;
+    char res[200];
+    FILE* f;
+    cout << "Введите слово: ";
+    cin >> term;
+
+    fopen_s(&f, "db.txt", "r");
+
+    while (true) {
+        fgets(res, 199, f);
+        string temp = res;
+        if (feof(f) != 0) {
+            break;
+        }
+        if (term[term.length() - 2] == temp[temp.length() - 3] &&
+            term[term.length() - 1] == temp[temp.length() - 2]) {
+            cout << temp;
+        }
+    }
+    fclose(f);
 }

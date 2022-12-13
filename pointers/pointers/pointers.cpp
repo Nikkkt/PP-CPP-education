1dfa45772a70b8e19b68626b195e7c1c85525e19
Nikkkt <work.terpilovsky@gmail.com>
2022-12-14 01:35:44 +0200

    Procedural programming in C++

--- pointers/pointers/pointers.cpp
+++ pointers/pointers/pointers.cpp
@@ -1,6 +1,10 @@
 ﻿#include <iostream>
 using namespace std;
 
+void first_task(int* arr1, int* arr2, int size1, int size2);
+void second_task(int* arr, int size, int num);
+void third_task(int* arr, int size, int index);
+
 int main() {
     srand(time(NULL));
 
@@ -79,78 +83,162 @@ int main() {
     //////////////////////////////////////////////////////////////
 
     //1
-    int size1;
-    cout << "Enter size of array: ";
+    //int size1;
+    //cout << "Enter size of array: ";
+    //cin >> size1;
+    //int* arr1 = new int[size1];
+    //int* arr2 = arr1;
+    //for (int i = 0; i < size1; i++) {
+    //    *(arr1 + i) = rand() % 100;
+    //}
+    //for (int i = 0; i < size1; i++) {
+    //    *(arr2 + size1 - i - 1) = *(arr1 + i);
+    //}
+    //cout << "Array 1: ";
+    //for (int i = 0; i < size1; i++) {
+    //    cout << *(arr1 + i) << "\t";
+    //}
+    //cout << endl << "Array 2: ";
+    //for (int i = 0; i < size1; i++) {
+    //    cout << *(arr2 + i) << "\t";
+    //}
+    //cout << endl;
+
+    ////2
+    //int size2;
+    //cout << "Enter size of array: ";
+    //cin >> size2;
+    //int* arr3 = new int[size2];
+    //for (int i = 0; i < size2; i++) {
+    //    *(arr3 + i) = rand() % 100;
+    //}
+    //int min = *arr3, max = *arr3;
+    //for (int i = 0; i < size2; i++) {
+    //    if (min > *(arr3 + i)) {
+    //        min = *(arr3 + i);
+    //    }
+    //    if (max < *(arr3 + i)) {
+    //        max = *(arr3 + i);
+    //    }
+    //}
+    //cout << endl << "Array: ";
+    //for (int i = 0; i < size2; i++) {
+    //    cout << *(arr3 + i) << "\t";
+    //}
+    //cout << endl << "Min: " << min << endl << "Max: " << max << endl << endl;
+
+    ////3
+    //int size3;
+    //cout << "Enter size of array: ";
+    //cin >> size3;
+
+    //int* arr4 = new int[size3];
+    //int bias;
+
+    //cout << "Please, enter the bias: ";
+    //cin >> bias;
+
+    //for (int i = 0; i < size3; i++) {
+    //    *(arr4 + i) = rand() % 100;
+    //}
+    //cout << "Old array: ";
+    //for (int i = 0; i < size3; i++) {
+    //    cout << *(arr4 + i) << "\t";
+    //}
+
+    //for (int j = 0; j < bias; j++) {
+    //    int tmp = *(arr4 + size3 - 1);
+    //    for (int i = size3 - 2; i >= 0; i--) {
+    //        *(arr4 + i + 1) = *(arr4 + i);
+    //    }
+    //    *arr4 = tmp;
+    //}
+    //cout << endl << "New array: ";
+    //for (int i = 0; i < size3; i++) {
+    //    cout << *(arr4 + i) << "\t";
+    //}
+
+    //////////////////////////////////////////////////////////////
+    ///////////////////////// 10.12.22 ///////////////////////////
+    //////////////////////////////////////////////////////////////
+
+    //1
+    int size1, size2;
     cin >> size1;
-    int* arr1 = new int[size1];
-    int* arr2 = arr1;
+    cin >> size2;
+    int* first = new int[size1];
     for (int i = 0; i < size1; i++) {
-        *(arr1 + i) = rand() % 100;
-    }
-    for (int i = 0; i < size1; i++) {
-        *(arr2 + size1 - i - 1) = *(arr1 + i);
-    }
-    cout << "Array 1: ";
-    for (int i = 0; i < size1; i++) {
-        cout << *(arr1 + i) << "\t";
-    }
-    cout << endl << "Array 2: ";
-    for (int i = 0; i < size1; i++) {
-        cout << *(arr2 + i) << "\t";
+        first[i] = rand() % 100;
+        cout << first[i] << "\t";
     }
     cout << endl;
+    int* second = new int[size2];
+    for (int i = 0; i < size2; i++) {
+        second[i] = rand() % 100;
+        cout << second[i] << "\t";
+    }
+    cout << endl;
+    first_task(first, second, size1, size2);
 
     //2
-    int size2;
-    cout << "Enter size of array: ";
-    cin >> size2;
-    int* arr3 = new int[size2];
-    for (int i = 0; i < size2; i++) {
-        *(arr3 + i) = rand() % 100;
+    int size3;
+    cin >> size3;
+    int* third = new int[size3];
+    for (int i = 0; i < size3; i++) {
+        third[i] = rand() % 100;
+        cout << third[i] << "\t";
     }
-    int min = *arr3, max = *arr3;
-    for (int i = 0; i < size2; i++) {
-        if (min > *(arr3 + i)) {
-            min = *(arr3 + i);
-        }
-        if (max < *(arr3 + i)) {
-            max = *(arr3 + i);
-        }
-    }
-    cout << endl << "Array: ";
-    for (int i = 0; i < size2; i++) {
-        cout << *(arr3 + i) << "\t";
-    }
-    cout << endl << "Min: " << min << endl << "Max: " << max << endl << endl;
+    cout << endl;
+    int rand_num = rand() % 100;
+    second_task(third, size3, rand_num);
 
     //3
-    int size3;
-    cout << "Enter size of array: ";
-    cin >> size3;
-
-    int* arr4 = new int[size3];
-    int bias;
-
-    cout << "Please, enter the bias: ";
-    cin >> bias;
-
-    for (int i = 0; i < size3; i++) {
-        *(arr4 + i) = rand() % 100;
+    int size4;
+    cin >> size4;
+    int* fourth = new int[size4];
+    for (int i = 0; i < size4; i++) {
+        fourth[i] = rand() % 100;
+        cout << fourth[i] << "\t";
     }
-    cout << "Old array: ";
-    for (int i = 0; i < size3; i++) {
-        cout << *(arr4 + i) << "\t";
-    }
+    cout << endl;
+    third_task(fourth, size4, 5);
+}
 
-    for (int j = 0; j < bias; j++) {
-        int tmp = *(arr4 + size3 - 1);
-        for (int i = size3 - 2; i >= 0; i--) {
-            *(arr4 + i + 1) = *(arr4 + i);
-        }
-        *arr4 = tmp;
+void first_task(int* arr1, int* arr2, int size1, int size2) {
+    int *arr_res = new int[size1 + size2];
+    for (int i = 0; i < size1; i++) {
+        *arr_res++ = arr1[i];
     }
-    cout << endl << "New array: ";
-    for (int i = 0; i < size3; i++) {
-        cout << *(arr4 + i) << "\t";
+    for (int i = 0; i < size2; i++) {
+        *arr_res++ = arr2[i];
     }
+    arr_res -= size1 + size2;
+    for (int i = 0; i < size1 + size2; i++) {
+        cout << *arr_res++ << "\t";
+    }
+    cout << endl;
+}
+
+void second_task(int* arr, int size, int num) {
+    int* arr_res = new int[size + 1];
+    for (int i = 0; i < size; i++) {
+        *arr_res++ = arr[i];
+    }
+    *arr_res++ = num;
+    arr_res -= size + 1;
+    for (int i = 0; i < size + 1; i++) {
+        cout << *arr_res++ << "\t";
+    }
+    cout << endl;
+}
+
+void third_task(int* arr, int size, int index) {
+    for (int i = index - 1; i < size; i++) {
+        arr[i] = arr[i + 1];
+    }
+    size--;
+    for (int i = 0; i < size; i++) {
+        cout << *arr++ << "\t";
+    }
+    cout << endl;
 }
